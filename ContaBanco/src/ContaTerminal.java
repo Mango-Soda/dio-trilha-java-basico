892ff2a4dcbba50a5dc77322964789c17b90c863
Mango Soda <4loneshimono@gmail.com>
2025-03-24 22:22:03 -0300

    Excluindo arquivo App.java

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -26,4 +26,17 @@ public class ContaTerminal {
         System.out.println(mensagem);
         return scanner.nextLine();
     }
+
+    public static void main(String[] args) throws Exception {
+        ContaTerminal conta = new ContaTerminal(0, null, null, 0);
+
+        conta.receberDados();
+
+        String mensagem = String.format(
+        "Olá %s, obrigado por criar uma conta no nosso banco. Sua agência é %s, conta %d e seu saldo é R$ %.2f.",
+        conta.nomeCliente, conta.agencia, conta.numero, conta.saldo
+        );
+
+        System.out.println(mensagem);
+    }
 }
