07ef68f83ac6a847689c909fefdeabd6db97964b
Mango Soda <4loneshimono@gmail.com>
2025-03-24 22:14:14 -0300

    Adicionando meu projeto Conta Banco

---
+++ ContaBanco/src/ContaTerminal.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class ContaTerminal {
+    int numero;
+    String agencia;
+    String nomeCliente;
+    float saldo;
+
+    public ContaTerminal(int numero, String agencia, String nomeCliente, float saldo)
+    {
+        this.agencia = agencia;
+        this.nomeCliente = nomeCliente;
+        this.saldo = saldo;
+    }
+    
+    public ContaTerminal receberDados() throws Exception
+    {
+        Scanner scanner = new Scanner(System.in);
+        this.numero = Integer.parseInt(lerEntrada(scanner, "Digite o número da conta bancária:"));
+        this.agencia = lerEntrada(scanner, "Digite o nome da agência:");
+        this.nomeCliente = lerEntrada(scanner, "Digite o nome do cliente:");
+        this.saldo = Float.parseFloat(lerEntrada(scanner, "Insira o saldo a ser depositado:"));
+
+        return new ContaTerminal(numero, agencia, nomeCliente, saldo);
+    }
+
+    public String lerEntrada(Scanner scanner, String mensagem)
+    {
+        System.out.println(mensagem);
+        return scanner.nextLine();
+    }
+}
